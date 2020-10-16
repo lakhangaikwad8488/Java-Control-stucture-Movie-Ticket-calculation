3cadaf10bf3962a8395ee692db3c6b1601f96e76
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-10-16 14:06:02 +0530

    Update CinemaTicket.java

--- src/main/java/main/java/yaksha/CinemaTicket.java
+++ src/main/java/main/java/yaksha/CinemaTicket.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CinemaTicket {
 
 	public double calculateCost(int no, String ref, String cou, String cir) {
-		/* You code Here */  
+		/* You code Here and find total cost according to given condition */  
 		return 0;
 	}
 
